51cf5598ba50734e225b38040ce029537a5ecde6
Scott Morey <swmorey@gmail.com>
2013-12-03 14:30:50 -0500

    now trying to auto-expire. We will probably create a seperate task for this in production that also autorefreshes

--- app/controllers/drupal_controller.rb
+++ app/controllers/drupal_controller.rb
@@ -8,14 +8,15 @@ require 'oj'
 require 'nikkou'
 require 'dalli'
 require 'memcachier'
-caches_action :test
+caches_action :test, :expires_in => 5.minutes
 
 def test
 headers['Access-Control-Allow-Origin'] = "*"
+timestamp = Time.now.to_s
 featured_fiction = JSON.parse(open("https://www.tadl.org/mobile/export/items/67/json").read)
 featured_nonfiction = JSON.parse(open("https://www.tadl.org/mobile/export/items/68/json").read)
 respond_to do |format|
-format.json { render :json => { :featured_fiction => featured_fiction, :featured_nonfiction => featured_nonfiction }}
+format.json { render :json => { :time => timestamp, :featured_fiction => featured_fiction, :featured_nonfiction => featured_nonfiction }}
 end
 
 end
