69e14acad0990c2e5614674a1c47c34e4c129a6c
Scott Morey <swmorey@gmail.com>
2013-12-03 17:49:43 -0500

    added all feeds used for the landing page... I think. Gonna test performance

--- app/controllers/drupal_controller.rb
+++ app/controllers/drupal_controller.rb
@@ -15,8 +15,17 @@ headers['Access-Control-Allow-Origin'] = "*"
 timestamp = Time.now.to_s
 featured_fiction = JSON.parse(open("https://www.tadl.org/mobile/export/items/67/json").read)
 featured_nonfiction = JSON.parse(open("https://www.tadl.org/mobile/export/items/68/json").read)
+new_music = JSON.parse(open("https://www.tadl.org/mobile/export/items/29/json").read)
+featured_news = JSON.parse(open("https://www.tadl.org/export/news/json").read)
+events = JSON.parse(open("https://www.tadl.org/mobile/export/events/formatted/json/all").read)
 respond_to do |format|
-format.json { render :json => { :time => timestamp, :featured_fiction => featured_fiction, :featured_nonfiction => featured_nonfiction }}
+format.json { render :json => { :time => timestamp, 
+	:featured_fiction => featured_fiction, 
+	:featured_nonfiction => featured_nonfiction, 
+	:new_music => new_music, 
+	:featured_news => featured_news,
+	:events => events,
+	}}
 end
 
 end
