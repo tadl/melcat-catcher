63110251a29e206cf8a88ffeeacd8d6d73238851
Scott Morey <swmorey@gmail.com>
2013-12-09 17:27:06 -0500

    added action to set cache headers

--- app/controllers/drupal_controller.rb
+++ app/controllers/drupal_controller.rb
@@ -10,19 +10,20 @@ require 'dalli'
 require 'memcachier'
 require 'timeout'
 
+before_filter :set_cache_headers, :only => [:drupal] 
 caches_action :drupal, :expires_in => 9.minutes, :race_condition_ttl => 1.minutes
 
 def drupal
-headers['Access-Control-Allow-Origin'] = '*'
-headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-headers['Access-Control-Request-Method'] = '*'
-headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 payload = Rails.cache.read('test')
 respond_to do |format|
 format.json { render :json => payload }
 end
 end
 
+
+def set_cache_headers
+    headers['Access-Control-Allow-Origin'] = '*'      
+end
 
 
 
