15573b54a6f9ec74eee0a3424638f72b810f2ecd
Scott Morey <swmorey@gmail.com>
2013-12-09 16:45:52 -0500

    trying to resolve issue with CORS

--- app/controllers/drupal_controller.rb
+++ app/controllers/drupal_controller.rb
@@ -13,7 +13,10 @@ require 'timeout'
 caches_action :drupal, :expires_in => 9.minutes, :race_condition_ttl => 1.minutes
 
 def drupal
-headers['Access-Control-Allow-Origin'] = "*"
+headers['Access-Control-Allow-Origin'] = '*'
+headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+headers['Access-Control-Request-Method'] = '*'
+headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 payload = Rails.cache.read('test')
 respond_to do |format|
 format.json { render :json => payload }
