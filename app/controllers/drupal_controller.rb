63475b1298056996e4ec995f0da9621f68d55a21
Scott Morey <swmorey@gmail.com>
2013-12-09 15:13:09 -0500

    added headers to allow for cross domain fun time

--- app/controllers/drupal_controller.rb
+++ app/controllers/drupal_controller.rb
@@ -13,6 +13,7 @@ require 'timeout'
 caches_action :drupal, :expires_in => 9.minutes, :race_condition_ttl => 1.minutes
 
 def drupal
+headers['Access-Control-Allow-Origin'] = "*"
 payload = Rails.cache.read('test')
 respond_to do |format|
 format.json { render :json => payload }
