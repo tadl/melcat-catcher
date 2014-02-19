3c3274e94c9539c125af8fcb985c008c53426227
Scott Morey <swmorey@gmail.com>
2014-02-19 17:56:48 -0500

    changing result size did not yeild compelling improvment. changing up facet handling.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -200,7 +200,7 @@ end
   
 if params[:q].present? 
 
-@pagetitle = 'http://catalog.tadl.org/eg/opac/results?query=' + @searchqueryclearned + '' +  @searchby + '&fi%3A'+ @mediatype +''+ @avail +'&locg='+ @loc +'&limit=20'+ @sorttype +'&page='+ @nextpage +'&'+@facet 
+@pagetitle = 'http://catalog.tadl.org/eg/opac/results?query=' + @searchqueryclearned + '' +  @searchby + '&fi%3A'+ @mediatype +''+ @avail +'&locg='+ @loc +'&limit=20'+ @sorttype +'&page='+ @nextpage + @facet 
 url = @pagetitle
 @doc = Nokogiri::HTML(open(url))
 @pagenumber = @doc.at_css(".results-paginator-selected").text rescue nil
